b78ef25f53ead85da1eb8149b19d763d7a56402c
Alfredo Sumaran <a.sumaran@gmail.com>
2025-07-05 21:30:40 -0500

    Setup NestJS

--- apps/api/index.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello World from the API");
-console.log("API is running on port 3001 (future)");
---
+++ apps/api/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3001);
+  console.log(
+    'Hello World from NestJS API running on http://localhost:3001 🚀',
+  );
+}
+bootstrap();
