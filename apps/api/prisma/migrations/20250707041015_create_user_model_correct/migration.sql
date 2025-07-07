8c0dfaec68774c249b52fe1b2b0d200ef8aa153e
Alfredo Sumaran <a.sumaran@gmail.com>
2025-07-07 00:06:56 -0500

    Add docker support

--- apps/api/prisma/migrations/20250707041015_create_user_model_correct/migration.sql
+++ apps/api/prisma/migrations/20250707041015_create_user_model_correct/migration.sql
@@ -2,7 +2,6 @@
 CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
-    "username" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "firstName" TEXT,
     "lastName" TEXT,
@@ -17,13 +16,7 @@ CREATE TABLE "users" (
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
-
--- CreateIndex
 CREATE INDEX "users_email_idx" ON "users"("email");
-
--- CreateIndex
-CREATE INDEX "users_username_idx" ON "users"("username");
 
 -- CreateIndex
 CREATE INDEX "users_isActive_idx" ON "users"("isActive");
